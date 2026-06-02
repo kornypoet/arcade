8e746210b5d00e4cc2c686b4c54f0c10df1fac6f
kornypoet <dempsey.travis@gmail.com>
2026-06-01 21:47:17 -0500

    working with keyd

--- ES-DE/scripts/game-start/03-remap-keys.sh
+++ ES-DE/scripts/game-start/03-remap-keys.sh
@@ -6,10 +6,13 @@ wine_games=(
     )
 
 name="${1##*/}"
+conf_dir='/home/travis/arcade/keyd'
 
 for game in "${wine_games[@]}"; do
     if [ "${name}" = "${game}" ]; then
 	echo "Remapping ${game} using keyd"
+	
+	ln -sf "${conf_dir}/${game%.*}.conf" "${conf_dir}/default.conf"
 	sudo systemctl start keyd
     fi
 done
